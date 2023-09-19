b43fb3512aa341df7f34306521ad455505017a05
shobhit0023 <94340370+shobhit0023@users.noreply.github.com>
2023-09-19 22:57:15 +0530

    Add files via upload

---
+++ Leetcode/Remove_all_occurences_of_substring.cpp
@@ -0,0 +1,13 @@
+LeetCode:1910
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        int pos=s.find(part);
+        while(pos != string::npos){
+            s.erase(pos,part.length());
+            pos=s.find(part);
+        }
+        return s;
+        
+    }
+};
