62001ab2cf44f72ef5b49d013c0b42d474f50757
shobhit0023 <94340370+shobhit0023@users.noreply.github.com>
2023-10-01 12:59:15 +0530

    Add files via upload

---
+++ Leetcode/Isomorphic_Strings.cpp
@@ -0,0 +1,21 @@
+Leetcode 205
+
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        int hash[256]={0};
+        bool ischar[256]={0};
+        for(int i=0;i<s.size();i++){
+            if(hash[s[i]]==0 && ischar[t[i]]==0){
+                hash[s[i]]=t[i];
+                ischar[t[i]]=true;
+            }
+        }
+        for(int i=0;i<s.size();i++){
+            if(char(hash[s[i]])!=t[i])   {
+                return false;
+            } 
+        }
+        return true;
+    }
+};
